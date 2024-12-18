c15d9e71a5b58d0f6ef52de0dda6e064847a910a
takuya.t <shoori0621@gmail.com>
2024-12-18 22:32:32 +0900

    initialize project

--- run_on_wasabi.sh
+++ run_on_wasabi.sh
@@ -23,8 +23,7 @@ if [ -d $OS_PATH ]
 then
   echo $OS_PATH" exists"
   echo "pulling new changes..."
-
-cd $OS_PATH
+  cd $OS_PATH
   git pull origin for_saba
 else
   echo $OS_PATH" doesn't exist"
@@ -39,10 +38,8 @@ cd $HOME_PATH
 # Makefileをダウンロードする
 if [ ! -f $MAKEFILE_PATH ]; then
   echo "downloading Makefile..."
-  wget https://raw.githubusercontent.com/hikalium/wasabi/main/external_app_template/
-  Makefile
+  wget https://raw.githubusercontent.com/hikalium/wasabi/for_saba/external_app_template/Makefile
 fi
 
 make build
 $OS_PATH/scripts/run_with_app.sh ./target/x86_64-unknown-none/release/$APP_NAME
-
