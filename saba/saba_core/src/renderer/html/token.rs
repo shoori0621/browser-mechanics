39c6e7649e1a44259692e4124c1640390a002fd2
takuya.t <shoori0621@gmail.com>
2025-01-04 17:26:25 +0900

    HtmlTokenier実装 State::TagName

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -50,6 +50,32 @@ impl HtmlTokenizer {
     self.reconsume = false;
     self.input[self.pos - 1]
   }
+
+  fn append_tag_name(&mut self, c: char) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          ref mut tag,
+          self_closing: _,
+          attributes: _,
+        }
+        | HtmlToken::EndTag { ref mut tag } => tag.push(c),
+        _ => panic!("`latest_token` should be either StartTage or EndTag"),
+      }
+    }
+  }
+
+  fn take_latest_token(&mut self) -> Option<HtmlToken> {
+    assert!(self.latest_token.is_some());
+
+    let t = self.latest_token.as_ref().cloned();
+    self.latest_token = None;
+    assert!(self.latest_token.is_none());
+
+    t
+  }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -124,6 +150,7 @@ impl Iterator for HtmlTokenizer {
         State::Data => {
           if c == '<' {
             self.state = State::TagOpen;
+
             continue;
           }
 
@@ -136,6 +163,7 @@ impl Iterator for HtmlTokenizer {
         State::TagOpen => {
           if c == '/' {
             self.state = State::EndTagOpen;
+
             continue;
           }
 
@@ -143,6 +171,7 @@ impl Iterator for HtmlTokenizer {
             self.reconsume = true;
             self.state = State::TagName;
             self.create_tag(true);
+
             continue;
           }
 
@@ -159,8 +188,40 @@ impl Iterator for HtmlTokenizer {
             self.reconsume = true;
             self.state = State::TagName;
             self.create_tag(false);
+
             continue;
           }
+        }
+        State::TagName => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+
+            return self.take_latest_token();
+          }
+
+          if c.is_ascii_uppercase() {
+            self.append_tag_name(c.to_ascii_lowercase());
+
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_tag_name(c);
         }
         _ => {}
       }
