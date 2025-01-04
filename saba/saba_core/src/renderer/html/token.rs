a3498d4866318035db9189d8a3559e69939a9194
takuya.t <shoori0621@gmail.com>
2025-01-04 17:55:27 +0900

    HtmlTokenier実装 State::BeforeAttributeValue

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -323,6 +323,27 @@ impl Iterator for HtmlTokenizer {
           self.state = State::AttributeName;
           self.start_new_attribute();
         }
+        State::BeforeAttributeValue => {
+          if c == ' ' {
+            // 空白は無視する
+            continue;
+          }
+
+          if c == '"' {
+            self.state = State::AttributeValueDoubleQuoted;
+
+            continue;
+          }
+
+          if c == '\'' {
+            self.state = State::AttributeValueSingleQuoted;
+
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeValueUnquoted;
+        }
         _ => {}
       }
     }
