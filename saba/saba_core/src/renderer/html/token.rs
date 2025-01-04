242b0eeb1a395d32e307b1a5ae6b53105052e548
takuya.t <shoori0621@gmail.com>
2025-01-04 17:48:22 +0900

    HtmlTokenier実装 State::AttributeName

--- saba/saba_core/src/renderer/html/token.rs
+++ saba/saba_core/src/renderer/html/token.rs
@@ -91,6 +91,26 @@ impl HtmlTokenizer {
       }
     }
   }
+
+  fn append_attribute(&mut self, c: char, is_name: bool) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          self_closing: _,
+          ref mut attributes,
+        } => {
+          let len = attributes.len();
+          assert!(len > 0);
+
+          attributes[len - 1].add_char(c, is_name);
+        }
+        _ => panic!("`latest_token` should be either StartTag"),
+      }
+    }
+  }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -249,6 +269,27 @@ impl Iterator for HtmlTokenizer {
           self.reconsume = true;
           self.state = State::AttributeName;
           self.start_new_attribute();
+        }
+        State::AttributeName => {
+          if c == ' ' || c == '/' || c == '>' || self.is_eof() {
+            self.reconsume = true;
+            self.state = State::AfterAttributeName;
+            self.start_new_attribute();
+          }
+
+          if c == '=' {
+            self.state = State::BeforeAttributeValue;
+
+            continue;
+          }
+
+          if c.is_ascii_uppercase() {
+            self.append_attribute(c.to_ascii_lowercase(), /* is_name */ true);
+
+            continue;
+          }
+
+          self.append_attribute(c, /* is_name */ true);
         }
         _ => {}
       }
