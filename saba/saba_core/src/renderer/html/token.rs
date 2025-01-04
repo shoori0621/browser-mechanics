fa6b23e1345987c6b38b8c0d33806c32ef6513a9
takuya.t <shoori0621@gmail.com>
2025-01-04 12:03:38 +0900

    HtmlTokenier実装 EndTagOpenまで

---
+++ saba/saba_core/src/renderer/html/token.rs
@@ -0,0 +1,169 @@
+use crate::renderer::html::attribute::Attribute;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+  state: State,
+  pos: usize,
+  reconsume: bool,
+  latest_token: Option<HtmlToken>,
+  input: Vec<char>,
+  buf: String,
+}
+
+impl HtmlTokenizer {
+  pub fn new(html: String) -> Self {
+    Self {
+      state: State::Data,
+      pos: 0,
+      reconsume: false,
+      latest_token: None,
+      input: html.chars().collect(),
+      buf: String::new(),
+    }
+  }
+
+  fn is_eof(&self) -> bool {
+    self.pos >= self.input.len()
+  }
+
+  fn consume_next_input(&mut self) -> char {
+    let c = self.input[self.pos];
+    self.pos += 1;
+    c
+  }
+
+  fn create_tag(&mut self, start_tag_token: bool) {
+    if start_tag_token {
+      self.latest_token = Some(HtmlToken::StartTag {
+        tag: String::new(),
+        self_closing: false,
+        attributes: Vec::new(),
+      });
+    } else {
+      self.latest_token = Some(HtmlToken::EndTag { tag: String::new() });
+    }
+  }
+
+  fn reconsume_input(&mut self) -> char {
+    self.reconsume = false;
+    self.input[self.pos - 1]
+  }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+  StartTag {
+    tag: String,
+    self_closing: bool,
+    attributes: Vec<Attribute>,
+  },
+  EndTag {
+    tag: String,
+  },
+  Char(char),
+  Eof,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum State {
+  /// https://html.spec.whatwg.org/multipage/parsing.html#data-state
+  Data,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#tag-open-state
+  TagOpen,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#end-tag-open-state
+  EndTagOpen,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#tag-name-state
+  TagName,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-name-state
+  BeforeAttributeName,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-name-state
+  AttributeName,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-name-state
+  AfterAttributeName,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-value-state
+  BeforeAttributeValue,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state
+  AttributeValueDoubleQuoted,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(single-quoted)-state
+  AttributeValueSingleQuoted,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(unquoted)-state
+  AttributeValueUnquoted,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-value-(quoted)-state
+  AfterAttributeValueQuoted,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#self-closing-start-tag-state
+  SelfClosingStartTag,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-state
+  ScriptData,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-less-than-sign-state
+  ScriptDataLessThanSign,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-open-state
+  ScriptDataEndTagOpen,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-name-state
+  ScriptDataEndTagName,
+  /// https://html.spec.whatwg.org/multipage/parsing.html#temporary-buffer
+  TemporaryBuffer,
+}
+
+impl Iterator for HtmlTokenizer {
+  type Item = HtmlToken;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.pos >= self.input.len() {
+      return None;
+    }
+
+    loop {
+      let c = match self.reconsume {
+        true => self.reconsume_input(),
+        false => self.consume_next_input(),
+      };
+
+      match self.state {
+        State::Data => {
+          if c == '<' {
+            self.state = State::TagOpen;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          return Some(HtmlToken::Char(c));
+        }
+        State::TagOpen => {
+          if c == '/' {
+            self.state = State::EndTagOpen;
+            continue;
+          }
+
+          if c.is_ascii_alphabetic() {
+            self.reconsume = true;
+            self.state = State::TagName;
+            self.create_tag(true);
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+        }
+        State::EndTagOpen => {
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          if c.is_ascii_alphabetic() {
+            self.reconsume = true;
+            self.state = State::TagName;
+            self.create_tag(false);
+            continue;
+          }
+        }
+        _ => {}
+      }
+    }
+  }
+}
